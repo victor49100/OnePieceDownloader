f6ec1735c33e1b739eb5eed1914c7c7729afd344
victor <moreauvictor1906@gmail.com>
2023-03-07 18:52:14 +0100

    Ajout ComboBox

--- MangaDowloader2/Form1.Designer.cs
+++ MangaDowloader2/Form1.Designer.cs
@@ -29,38 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MangaDownloader));
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LabelUrl = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.labelTest = new System.Windows.Forms.Label();
+            this.LabelFolder = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // labelTitre
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label1.Location = new System.Drawing.Point(337, 45);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Manga Downloader 2";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelTitre.Font = new System.Drawing.Font("Kabel Bd", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitre.Location = new System.Drawing.Point(292, 30);
+            this.labelTitre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(198, 22);
+            this.labelTitre.TabIndex = 0;
+            this.labelTitre.Text = "Manga Downloader 2";
             // 
-            // textBox1
+            // textBoxUrl
             // 
-            this.textBox1.Location = new System.Drawing.Point(68, 263);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(331, 23);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxUrl.Location = new System.Drawing.Point(21, 274);
+            this.textBoxUrl.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(331, 23);
+            this.textBoxUrl.TabIndex = 1;
+            this.textBoxUrl.TextChanged += new System.EventHandler(this.TextBoxUrl);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(157, 292);
+            this.button1.Location = new System.Drawing.Point(110, 303);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 42);
@@ -69,20 +73,20 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Télecharger);
             // 
-            // label2
+            // LabelUrl
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.DarkGray;
-            this.label2.Location = new System.Drawing.Point(164, 232);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Télecharger via URL";
+            this.LabelUrl.AutoSize = true;
+            this.LabelUrl.BackColor = System.Drawing.Color.DarkGray;
+            this.LabelUrl.Location = new System.Drawing.Point(107, 228);
+            this.LabelUrl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelUrl.Name = "LabelUrl";
+            this.LabelUrl.Size = new System.Drawing.Size(133, 15);
+            this.LabelUrl.TabIndex = 3;
+            this.LabelUrl.Text = "Télecharger via URL";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(354, 79);
+            this.button2.Location = new System.Drawing.Point(337, 67);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(112, 48);
@@ -91,30 +95,75 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.ChangeLocation);
             // 
-            // labelTest
+            // LabelFolder
             // 
-            this.labelTest.AutoSize = true;
-            this.labelTest.Location = new System.Drawing.Point(252, 354);
-            this.labelTest.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelTest.Name = "labelTest";
-            this.labelTest.Size = new System.Drawing.Size(0, 15);
-            this.labelTest.TabIndex = 5;
-            this.labelTest.Click += new System.EventHandler(this.label3_Click);
+            this.LabelFolder.AutoSize = true;
+            this.LabelFolder.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LabelFolder.Location = new System.Drawing.Point(304, 129);
+            this.LabelFolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelFolder.Name = "LabelFolder";
+            this.LabelFolder.Size = new System.Drawing.Size(0, 15);
+            this.LabelFolder.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(470, 274);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 23);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "selectionnez un manga";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(485, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Télecharger via une liste";
+            this.label1.Click += new System.EventHandler(this.LabelCombo);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 303);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 42);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Télécharger";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.BoutonTelechargerCombo);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "aucun dossier selectionné";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // MangaDownloader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.BackgroundImage = global::MangaDowloader2.Properties.Resources._484b0617_d81e_11df_8228_a8bfc396a36f_gigapixel_standard_scale_4_00x;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(787, 541);
-            this.Controls.Add(this.labelTest);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(748, 541);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.LabelFolder);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.LabelUrl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.labelTitre);
             this.Font = new System.Drawing.Font("Kabel Bd", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -122,7 +171,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "MangaDownloader";
             this.Text = "MangaDownloader";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.c);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,12 +179,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelTitre;
+        private System.Windows.Forms.TextBox textBoxUrl;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LabelUrl;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Label LabelFolder;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- MangaDowloader2/Form1.cs
+++ MangaDowloader2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -9,37 +10,80 @@ using System.Drawing.Text;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 
 namespace MangaDowloader2
 {
 
+
     public partial class MangaDownloader : Form
     {
+        public Dictionary<string, string> UrlManga = new Dictionary<string, string>();
+        
+        //liste manga
 
         FolderBrowserDialog fbd = new FolderBrowserDialog();
         public MangaDownloader()
         {
+            //charge tt les items
             InitializeComponent();
+
+            // liste manga TODO remplacer par XML
+            UrlManga.Add("OnePiece", "https://lelscans.net/mangas/one-piece/{}/{}.jpg?v=fr1674202018");
+            UrlManga.Add("OnePiece2", "https://lelscans.net/mangas/one-piece/{}/{}.jpg?v=fr1674202018");
+            UrlManga.Add("OnePiece3", "https://lelscans.net/mangas/one-piece/{}/{}.jpg?v=fr1674202018");
+
+
+            comboBox1.DataSource = new BindingSource(UrlManga, null);
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
+            
+            
         }
 
+
+        //Transform url
+        public void TrimUrl(string url)
+
+
+        {
+            Regex regex = new Regex(@"\/(\d+)\.(jpg|jpeg|png|webp)(?:\?.*)?");
+            Match match = regex.Match(url);
+            if (match.Success)
+            {
+                string pageNumber = match.Groups[1].Value;
+                Debug.WriteLine($"Page number: {pageNumber}");
+            }
+            else
+            {
+                Debug.WriteLine("No match found.");
+            }
+        }
+
+
+
+        //boutton Telechargé
         public void DownloadFromUrl()
         {
-
+            int newChapterNumber = 1070;
             WebClient webClient = new WebClient();
             string k = "";
-            string folderPath = labelTest.Text;
+            string folderPath = "" + LabelFolder.Text + "\\";
 
             if (folderPath == "")
             {
-                labelTest.Text = "Aucun chemin renseigné !";
+                label2.Text = "Aucun chemin renseigné !";
             }
             else
             {
@@ -57,13 +101,13 @@ namespace MangaDowloader2
                     string fileName = string.Format(i + ".png");
                     string filePath = folderPath + fileName;
                     //string imageUrl = imageUrl1 + i + imageUrl2;
-                    string url = String.Format(@"https://lelscans.net/mangas/one-piece/1070/{0}.jpg?v=fr1674202018", k);
-
+                    string newUrl = string.Format("https://fr-scan.com/wp-content/uploads/WP-manga/data/manga_62b4957984def/eb0cb3fbac2d003454dbf5bab87efb65/0{0}.jpeg", i);
 
                     {
+                        //telecharge
                         try
                         {
-                            byte[] dataArr = webClient.DownloadData(url);
+                            byte[] dataArr = webClient.DownloadData(newUrl);
                             System.IO.File.WriteAllBytes(filePath, dataArr);
                         }
                         catch (Exception ex)
@@ -71,8 +115,8 @@ namespace MangaDowloader2
 
                         }
                     }
-                    labelTest.Text = url;
-                    Debug.WriteLine(url);
+                    LabelFolder.Text = newUrl;
+                    Debug.WriteLine(newUrl);
                     Debug.WriteLine(filePath);
 
 
@@ -86,20 +130,20 @@ namespace MangaDowloader2
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void ChangeLocation(object sender, EventArgs e)
         {
             if (fbd.ShowDialog() == DialogResult.OK)
             {
                 //Get the path of specified file
                 string folderPath = fbd.SelectedPath;
-                labelTest.Text = folderPath;
+                LabelFolder.Text = folderPath + "\\";
+                label2.Text = ("");
+
             }
         }
+
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -109,18 +153,55 @@ namespace MangaDowloader2
 
         private void Télecharger(object sender, EventArgs e)
         {
-            string textUrl = textBox1.Text;
+
             DownloadFromUrl();
         }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void TextBoxUrl(object sender, EventArgs e)
         {
 
+        }
+
+        private void c(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LabelCombo(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_ClickTrim(object sender, EventArgs e)
+        {
+            TrimUrl(textBoxUrl.Text);
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BoutonTelechargerCombo(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) return;
+            
         }
     }
 }
